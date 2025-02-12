594b01bb685d3dd5caf427f9274ff67008aabe04
jon1games <jonas@saslona.com>
2025-02-12 19:02:33 +0100

    option to not fill the empty slots

--- src/main/java/me/gaminglounge/guiapi/GuiFromMap.java
+++ src/main/java/me/gaminglounge/guiapi/GuiFromMap.java
@@ -21,10 +21,31 @@ public class GuiFromMap implements InventoryHolder {
     private Component inventoryName;
     private final int row;
     private Map<Integer, ItemStack> items;
+    private boolean fill;
 
+    /**
+     * 
+     * @param holder inventoryholder
+     * @param row    how many rows the gui shoud have
+     * 
+     */
     public GuiFromMap(InventoryHolder holder, int row) {
         this.row = row;
         this.holder = holder;
+        this.fill = true;
+    }
+
+    /**
+     * 
+     * @param holder inventoryholder
+     * @param row    how many rows the gui shoud have
+     * @param fill   if the empty slots should be filles, default = true
+     * 
+     */
+    public GuiFromMap(InventoryHolder holder, int row, boolean fill) {
+        this.row = row;
+        this.holder = holder;
+        this.fill = fill;
     }
 
     public GuiFromMap setInventoryName(Component inventoryName) {
@@ -50,20 +71,21 @@ public class GuiFromMap implements InventoryHolder {
             this.inv.setItem(entry, items.get(entry));
         }
 
-        int first = this.inv.firstEmpty();
-        if (first == -1)
-            return inv;
+        if (fill) {
+            int first = this.inv.firstEmpty();
+            if (first == -1)
+                return inv;
 
-        for (int i = first; i < inv.getSize(); i++) {
-            if (items.containsKey(i))
-                continue;
+            for (int i = first; i < inv.getSize(); i++) {
+                if (items.containsKey(i))
+                    continue;
 
-            this.inv.setItem(i,
-                    new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE)
-                            .addBothClickEvent("ItemBuilder:cancel")
-                            .build());
+                this.inv.setItem(i,
+                        new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE)
+                                .addBothClickEvent("ItemBuilder:cancel")
+                                .build());
+            }
         }
-
         return inv;
     }
 
